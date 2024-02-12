992fbc377c86555ea35c505ab77f91cb69f7fe22
Marijan Dupkovic <info@marijandupkovic.com>
2024-02-12 19:32:01 +0100

    Change_profile_img

--- bloom/user/forms.py
+++ bloom/user/forms.py
@@ -1,7 +1,8 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = '__all__'
+
