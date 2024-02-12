05b7b3d19ccc6cb158c73f20a4507e9bcb403e58
Marijan Dupkovic <info@marijandupkovic.com>
2024-02-12 19:56:11 +0100

    Change_img

--- bloom/user/utils.py
+++ bloom/user/utils.py
@@ -2,11 +2,12 @@ from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 
-from bloom.user.models import CustomUser
+from user.models import CustomUser
 
 @api_view(('put',))
 def change_profile_img(request, token):
     user = get_object_or_404(CustomUser, auth_token=token)
-    user.profile_picture = request.data.get('profile_picture')
+    print(request.data.get('profile_picture'))
+    user.profile_picture = request.fields.get('profile_picture')
     user.save()
     return Response({'message': 'User activated successfully.'})
